774087203ff6cae76fd9ab163a50fe5a570b95ea
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2025-04-05 19:09:00 +0100

    fix(links): heading links not handled correctly

--- src/view/components/container/column/components/group/components/card/components/content/event-handlers/handle-links/heading-link/handle-local-heading-link.ts
+++ src/view/components/container/column/components/group/components/card/components/content/event-handlers/handle-links/heading-link/handle-local-heading-link.ts
@@ -2,16 +2,14 @@ import { LineageView } from 'src/view/view';
 import { selectCard } from 'src/view/components/container/column/components/group/components/card/components/content/event-handlers/handle-links/helpers/select-card';
 
 export const handleLocalHeadingLink = (view: LineageView, link: string) => {
-    const match = /(#.+)$/.exec(link);
+    const match = /#+(.*)$/.exec(link);
     if (!match) return;
-    const headingPart = match[1];
+    const headingText = match[1].trimStart();
     for (let level = 1; level <= 6; level++) {
         const headings = Array.from(
             view.containerEl.querySelectorAll('h' + level),
         ) as HTMLHeadingElement[];
-        const heading = headings.find(
-            (h) => '#' + h.dataset.heading === headingPart,
-        );
+        const heading = headings.find((h) => h.dataset.heading === headingText);
         if (heading) {
             const card = heading.closest('.lineage-card');
             if (card && card.id) {
